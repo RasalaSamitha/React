0418de5bfb5cddb5c733f0183ffff963bd190f88
akanksha-rasala <109146104+akanksha-rasala@users.noreply.github.com>
2025-05-17 19:19:45 +0530

    changes in counter app

--- 01_CounterApp/src/App.jsx
+++ 01_CounterApp/src/App.jsx
@@ -10,7 +10,21 @@ function App() {
   const AddValue = () =>{
     console.log(counter);
     if(counter < 20)
-    setCounter(counter+1);
+       setCounter(counter+1);
+      
+  //this will not update counter value 4 times updates only 1 time 
+  // setCounter(counter+1);
+  // setCounter(counter+1);
+  // setCounter(counter+1);
+  // setCounter(counter+1);
+  // setCounter(counter+1);
+
+  //this updates counter value 5 times bzc it takes previous value 
+  // setCounter((pre) => pre + 1);
+  // setCounter((pre) => pre + 1);
+  // setCounter((pre) => pre + 1);
+  // setCounter((pre) => pre + 1);
+  // setCounter((pre) => pre + 1);
   }
 
   const removeValue = () => {
